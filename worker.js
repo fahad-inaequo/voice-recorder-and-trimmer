68b2a3d2ec49f7133ddc5f43598f33302960756b
Fahad Hussain <fahad.hussain@inaequo.net>
2022-07-19 11:57:01 +0500

    worker

--- worker.js
+++ worker.js
@@ -28,8 +28,6 @@
 		if(audioData.channels.length == 1) {
 			mp3Encoder = new lamejs.Mp3Encoder(1, audioData.sampleRate, 128);
 			return new Promise( (resolve, reject) => {
-				console.log(evt);
-
 				var arrayBuffer = audioData.channels[0];
 				var sampleRate = audioData.sampleRate;
 				var data = new Float32Array(arrayBuffer);
