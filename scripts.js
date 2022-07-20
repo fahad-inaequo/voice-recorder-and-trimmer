0a77ff5a3f4a5cfca76e8a30af523c2570c5fa4f
Fahad Hussain <fahad.hussain@inaequo.net>
2022-07-20 18:02:42 +0500

    trimming using buffer

--- scripts.js
+++ scripts.js
@@ -236,66 +236,46 @@ const stopRecording = () => {
 
 
 
-var app = function () {
-  debugger
-  var params = wavesurfer.regions.list[Object.keys(wavesurfer.regions.list)[0]]
-  var start = params.start;
-  var end = params.end;
+const myNewFun = () => {
+  let region = wavesurfer.regions.list[Object.keys(wavesurfer.regions.list)[0]]
+  //Create empty buffer and then put the slice of audioBuffer i.e wanted part
+  var startPoint = Math.floor((region.start * audioBuffer.length) / totalAudioDuration);
+  var endPoint = Math.ceil((region.end * audioBuffer.length) / totalAudioDuration);
+  var duration = endPoint - startPoint;
 
-  var originalAudioBuffer;
+  // create a new buffer to hold the new clip
+  var buffer = createBuffer(wavesurfer.backend.buffer, duration)
+  // copy
+  copyBuffer(wavesurfer.backend.buffer, start, end, buffer, 0)
 
-
-  originalAudioBuffer = params.wavesurfer.backend.buffer;
-
-  var lengthInSamples = Math.floor((end - start) *
-    originalAudioBuffer.sampleRate);
-
-  var offlineAudioContext = new webkitOfflineAudioContext(1, 2,
-    originalAudioBuffer.sampleRate);
-  var
-    new_channel_data, empty_segment_data, original_channel_data, before_data, after_data;
-
-  var emptySegment = offlineAudioContext.createBuffer(
-    originalAudioBuffer.numberOfChannels, lengthInSamples,
-    originalAudioBuffer.sampleRate);
-
-  var newAudioBuffer = offlineAudioContext.createBuffer(
-    originalAudioBuffer.numberOfChannels,
-    (start === 0 ? (originalAudioBuffer.length - emptySegment.length) :
-      originalAudioBuffer.length)
-    , originalAudioBuffer.sampleRate);
-
-  for (var channel = 0; channel < originalAudioBuffer.numberOfChannels;
-    channel++) {
-
-    new_channel_data = newAudioBuffer.getChannelData(channel);
-    empty_segment_data = emptySegment.getChannelData(channel);
-    original_channel_data = originalAudioBuffer.getChannelData(channel);
-
-    before_data = original_channel_data.subarray(0, start *
-      originalAudioBuffer.sampleRate);
-    after_data = original_channel_data.subarray(Math.floor(end *
-      originalAudioBuffer.sampleRate), (originalAudioBuffer.length *
-        originalAudioBuffer.sampleRate));
-
-    if (start > 0) {
-      new_channel_data.set(before_data);
-      new_channel_data.set(empty_segment_data, (start *
-        newAudioBuffer.sampleRate));
-      new_channel_data.set(after_data, (end * newAudioBuffer.sampleRate));
-    } else {
-      new_channel_data.set(after_data);
-    }
-
-  }
-
-  var arraybuffer = buffer2wav(newAudioBuffer);//Will create a new Blob with the IntArray...
-
-
-  return (new Blob([arraybuffer], { type: 'audio/wav' }));
-
+  // load the new buffer
+  wavesurfer.empty()
+  wavesurfer.loadDecodedBuffer(buffer)
 }
 
+function createBuffer(originalBuffer, duration) {
+  var sampleRate = originalBuffer.sampleRate
+  var frameCount = duration * sampleRate
+  var channels = originalBuffer.numberOfChannels
+  let buffer = new AudioContext().createBuffer(
+    channels,
+    frameCount,
+    sampleRate
+  );
+  return buffer
+}
+
+function copyBuffer(fromBuffer, fromStart, fromEnd, toBuffer, toStart) {
+  var sampleRate = fromBuffer.sampleRate
+  var frameCount = (fromEnd - fromStart) * sampleRate
+  for (var i = 0; i < fromBuffer.numberOfChannels; i++) {
+    var fromChanData = fromBuffer.getChannelData(i)
+    var toChanData = toBuffer.getChannelData(i)
+    for (var j = 0, f = Math.round(fromStart * sampleRate), t = Math.round(toStart * sampleRate); j < frameCount; j++, f++, t++) {
+      toChanData[t] = fromChanData[f]
+    }
+  }
+}
 
 function downloadMp3() {
   debugger
